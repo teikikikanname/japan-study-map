61e47798d95f345ac8750ea9ea22fd49abcdd5b1
teikikikanname <dahegantai@gmail.com>
2026-05-10 21:11:59 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 import streamlit as st
-import pandas as pd
 
-# 1. サイトのタイトル
+# Google Analytics 連携
+st.markdown("""
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-W9WDMKSB7S"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', 'G-W9WDMKSB7S');
+    </script>
+    """, unsafe_allow_html=True)
+
 st.title("📖 駅勉ガイド 神奈川版")
-
-# 2. 地図や難しい機能は一旦なしにして、文字だけ出します
-st.write("現在、サイトを修復中です。この文字が見えていれば成功です！")
-
-# 3. Googleへの合言葉（これさえあれば、サチコの確認ボタンは押せます）
-st.markdown('<meta name="google-site-verification" content="ROSJqr15YgcHGn7S5kq-OQJI0EGH47vCPUk9OnKAJXY" />', unsafe_allow_html=True)
-
-stations = ["横浜", "武蔵小杉", "小田原"]
-st.selectbox("駅を選んでください", stations)
+st.write("アナリティクス接続テスト中")
